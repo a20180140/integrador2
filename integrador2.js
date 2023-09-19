047738252e1adac30f9daaa6943da5ff920792b0
a20180140 <131192847+a20180140@users.noreply.github.com>
2023-09-19 12:43:24 -0500

    Add files via upload

---
+++ integrador2.js
@@ -0,0 +1,26 @@
+alert("bienvennida");
+nom=prompt("Esta es la triviva de primera, escriba por favor su nombre");
+
+Perg1=prompt("¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA: 6 meses de luz diurna ininterrumpida\nB: 6 meses de oscuridad inimterrumpida\nC: 6 meses de alternancia de luz y oscuridad");
+if(Perg1--){
+    preg1-25
+}
+else{
+    preg1=0
+}
+document.write("Tu puntaje es",preg1);
+
+Perg2=prompt("¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA: 6 meses de luz diurna ininterrumpida\nB: 6 meses de oscuridad inimterrumpida\nC: 6 meses de alternancia de luz y oscuridad");
+if(preg2--){
+    preg2-25
+}
+else{
+    preg2=0
+}
+total=preg1+preg2;
+if (total=25){
+    document.write(nom," tu ountajes es ",total,"sabes sobr ela primavera")
+}
+else{
+    document.write(nom,"tu púntajes es ",total,"te falta")
+}
